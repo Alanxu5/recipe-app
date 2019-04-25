91ff9e387e5c7dae6a6617aaed6edd68e225ee99
Alan Xu <ayx002@ucsd.edu>
2019-04-24 22:53:16 -0700

    adds url-loader to view static img, adds new font, changes design of recipe view page

--- src/App.vue
+++ src/App.vue
@@ -58,11 +58,17 @@ export default {
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Lato');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Condensed', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+html, body {
+  font-family: 'Roboto Condensed', sans-serif;
 }
 
 .header {
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -13,7 +13,7 @@ export default {
     // this.feeds = this.$route.params.recipeData.feeds;
 
     this.name = "World's Best Lasagna";
-    this.description = "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawd";
+    this.description = "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawd. Chase QuickPay(R) with Zelle(SM) STEPHANIE HUA requested $409.56 Go to Chase.com/QP or the Chase app to Send Money. Message: Splitwise 4/24";
     this.ingredients = "1 pound sweet Italian sausage\n3/4 pound lean ground beef\n1/2 cup minced onion\n2 cloves garlic, crushed";
     this.directions = "In a Dutch oven, cook sausage, ground beef, onion, and garlic over medium heat until well browned. Stir in crushed tomatoes, tomato paste, tomato sauce, and water. Season with sugar, basil, fennel seeds, Italian seasoning, 1 tablespoon salt, pepper, and 2 tablespoons parsley. Simmer, covered, for about 1 1/2 hours, stirring occasionally.\nBring a large pot of lightly salted water to a boil. Cook lasagna noodles in boiling water for 8 to 10 minutes. Drain noodles, and rinse with cold water. In a mixing bowl, combine ricotta cheese with egg, remaining parsley, and 1/2 teaspoon salt.";
     this.prepTime = "30 m";
@@ -25,68 +25,97 @@ export default {
 
 <template>
   <div class="recipe-container">
-    <h1>View Recipes</h1>
-    <div class="container">
-      <div class="summary">
-        <div class="image-container">
-          <img 
-            class="image"
-            src="https://cdn.fstoppers.com/styles/full/s3/media/2015/03/apple-iphone-6-world-gallery-fstoppers-alastair_b.png">
-        </div>
-        <div class="info">
-          <div class="title">
-            {{ name }}
+    <div>
+      <div class="container">
+        <div class="summary">
+          <div class="image-container">
+            <img 
+              class="image"
+              src="@/assets/images/IMG_1176.jpg">
           </div>
-          <div class="time">
-            <div>Prep: {{ prepTime }}</div>
-            <div>Cook: {{ cookTime }}</div>
-          </div>
-          <div>Number of meals: {{ feeds }}</div>
-          <div>Description: {{ description }}</div>
-        </div>        
-      </div>
-      <div class="instructions">
-        <div class="ingredients">
-          <div>{{ ingredients }}</div>
+          <div class="info">
+            <div class="title">
+              {{ name }}
+            </div>
+            <div class="subtext">
+              Prep: {{ prepTime }}
+            </div>
+            <div class="subtext"> 
+              Cook: {{ cookTime }}
+            </div>
+            <div class="subtext">
+              Servings: {{ feeds }}
+            </div>
+            <div class="subtext">
+              Preparation: Oven
+            </div>
+            <div class="description">
+              Description: {{ description }}
+            </div>
+          </div>        
         </div>
-        <div class="directions">
-          <div>{{ directions }}</div>
+        <div class="instructions">
+          <div class="ingredients">
+            <div class="section-header">
+              Ingredients
+            </div>
+            <div>{{ ingredients }}</div>
+          </div>
+          <div class="directions">
+            <div class="section-header">
+              Directions
+            </div>
+            <div>{{ directions }}</div>
+          </div>
         </div>
       </div>
     </div>
-    <RouterView />
+    <div class="other-recipes">
+      OTHER RECIPES
+    </div>
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .recipe-container {
-    margin: 30px 30px 30px 30px;
-    width: 80%;
+    margin: 3rem 4rem 3rem 7rem;
+    display: grid;
+    grid-template-columns: 75% 1fr;
+    grid-column-gap: 3rem;
   }
 
   .container {
     display: grid;
-    grid-template-rows: auto auto
+    grid-template-rows: auto auto;
+    grid-row-gap: 2rem;
   }
 
   .summary {
     display: grid;
-    grid-template-columns: 500px auto;
+    grid-template-columns: 400px 1fr;
+    grid-column-gap: 2rem;
   }
 
   .info {
+    display: grid;
+    grid-template-rows: auto 2rem 2rem 2rem 2rem 1fr;
+  }
 
+  .subtext {
+    margin-left: 3rem;
+    color: gray;
+  }
+
+  .description {
+    margin-left: 3rem;
   }
 
   .title {
-    font-size: 16px;
-  }
-
-  .time {
-    display: grid;
-    grid-template-columns: 40px 40px;
-    /* justify-content: center; */
+    font-size: 2.5rem;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 1rem;
   }
 
   .image-container {
@@ -94,20 +123,29 @@ export default {
   }
 
   .image {
-    width: 300px;
-    height: 300px;
+    width: 100%;
+    height: 400px;
   }
 
   .instructions {
     display: grid;
     grid-template-rows: auto auto;
+    row-gap: 2rem;
   }
 
   .ingredients {
 
   }
 
+  .section-header {
+    font-size: 1.5rem;
+  }
+
   .directions {
 
+  }
+
+  .other-recipes {
+    background-color: lightgray;
   }
 </style>
